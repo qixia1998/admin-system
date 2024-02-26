dfd761a8558aa010ded8fa12b7e23c69ff5ad740
qixia1998 <qixia717138552@gmail.com>
2024-02-26 22:35:46 +0800

    update auth

--- admin-api-golang/common/result/code.go
+++ admin-api-golang/common/result/code.go
@@ -4,22 +4,28 @@ package result
 
 // Codes 定义状态
 type Codes struct {
-	SUCCESS uint
-	FAILED  uint
-	Message map[uint]string
+	SUCCESS         uint
+	FAILED          uint
+	Message         map[uint]string
+	NOAUTH          uint
+	AUTHFORMATERROR uint
 }
 
 // ApiCode 状态码
 var ApiCode = &Codes{
-	SUCCESS: 200,
-	FAILED:  501,
+	SUCCESS:         200,
+	FAILED:          501,
+	NOAUTH:          403,
+	AUTHFORMATERROR: 405,
 }
 
 // 状态信息
 func init() {
 	ApiCode.Message = map[uint]string{
-		ApiCode.SUCCESS: "成功",
-		ApiCode.FAILED:  "失败",
+		ApiCode.SUCCESS:         "成功",
+		ApiCode.FAILED:          "失败",
+		ApiCode.NOAUTH:          "请求头中的auth为空",
+		ApiCode.AUTHFORMATERROR: "请求头中auth格式有误",
 	}
 }
 
